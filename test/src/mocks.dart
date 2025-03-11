695625cae5137bafaf8f8f4342e62256a6b4f376
Raul Alonzo Mendoza <ram2489@gmail.com>
2025-03-11 16:30:23 -0500

    Created all tests for the Flutter app
    Created unit tests for each repository or service, widget tests for the main Widgets, used the Widget tests in Golden tests and in the integration tests which are automated tests for the app using fake repositories.

---
+++ test/src/mocks.dart
@@ -0,0 +1,36 @@
+import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:piix_mobile/src/features/authentication/application/auth_service_barrel_file.dart';
+import 'package:piix_mobile/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:piix_mobile/src/features/authentication/presentation/common_widgets/verification_code_input/countdown_timer_controller.dart';
+import 'package:piix_mobile/src/network/app_dio.dart';
+
+/// Sets up the mocks for Firebase Core
+class MockFirebaseAuth extends Mock implements FirebaseAuth {}
+
+class MockUserCredential extends Mock implements UserCredential {}
+
+class MockFirebaseUser extends Mock implements User {}
+
+typedef Callback = void Function(MethodCall call);
+
+/// Sets up the mocks for Http Requests
+
+class MockDio extends Mock implements Dio {}
+
+class MockAppDio extends Mock implements AppDio {}
+
+/// Sets up the mocks for Authentication
+class MockAuthRepository extends Mock implements FakeAuthRepository {}
+
+class MockAuthService extends Mock implements FakeAuthService {}
+
+class MockFakeAuthService extends Mock implements FakeAuthService {}
+
+class MockResendCodeTimerNotifier extends Mock implements CountDownNotifier {}
+
+class Listener<T> extends Mock {
+  void call(T? previous, T next);
+}
